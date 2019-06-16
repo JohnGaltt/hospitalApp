10a8fd751df61c0342a2115c235ae725d1e76eb3
Oleksandr Zholob <johngaltaregoing@gmail.com>
2019-06-16 14:10:13 +0300

    Added initial data script

---
+++ db/InitialData.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments VALUES (1, 'Otorhinolaryngology', 322)
+INSERT INTO departments VALUES (2, 'Surgery', 552)
+
+INSERT INTO doctors VALUES (1, 'Vasyl Zvir', 'Proffesor', 1)
+
+INSERT INTO patients VALUES (1, 'Petro')
+
+INSERT INTO patientsummaries VALUES (1, 1, 'The patient is fully healthy', 1)
