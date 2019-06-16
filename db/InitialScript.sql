077e9f3fb423a00e71417d573237b3d15c01f9dd
Oleksandr Zholob <johngaltaregoing@gmail.com>
2019-06-16 14:07:13 +0300

    Added initial script for db

---
+++ db/InitialScript.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Doctors (
+Id  SERIAL PRIMARY KEY, 
+Name VARCHAR(32),
+DepartmentId INT,
+Position VARCHAR(32)
+)
+
+CREATE TABLE Patients (
+Id  SERIAL PRIMARY KEY, 
+Name VARCHAR(32)
+)
+
+CREATE TABLE Departments (
+Id SERIAL PRIMARY KEY,
+Name VARCHAR(32),
+DoorNumber INT
+)
+
+CREATE TABLE PatientSummaries (
+Id  SERIAL PRIMARY KEY, 
+DoctorId INT,
+Conclusion VARCHAR(128),
+PatientId INT
+)
+ALTER TABLE PatientSummaries 
+ADD CONSTRAINT FK_DoctorId 
+FOREIGN KEY(DoctorId)
+REFERENCES Doctors(Id)
+
+ALTER TABLE PatientSummaries 
+ADD CONSTRAINT FK_PatientId 
+FOREIGN KEY(PatientId)
+REFERENCES Patients(Id)
